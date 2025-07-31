35ba21cb33d5aea77fd4b4cefd97f2139e2cfc61
Paula Pniewska <pniewska.paula@gmail.com>
2025-07-31 15:55:07 +0200

    add details component and routing

--- .angular/cache/20.1.2/first-app/vite/deps/@angular_core.js
+++ .angular/cache/20.1.2/first-app/vite/deps/@angular_core.js
@@ -485,7 +485,7 @@ import {
   ɵɵvalidateIframeAttribute,
   ɵɵviewQuery,
   ɵɵviewQuerySignal
-} from "./chunk-IOK7PU67.js";
+} from "./chunk-SYHEP3OL.js";
 import "./chunk-WDMUDEB6.js";
 export {
   ANIMATION_MODULE_TYPE,
